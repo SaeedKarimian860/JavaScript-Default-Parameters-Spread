361e9e131aa1bd16ce39c03410bb748834c951a4
SaeedKarimian860 <SGK12190@gmail.com>
2024-10-16 22:50:53 -0400

    keyword this

--- main3.js
+++ main3.js
@@ -1,14 +1,14 @@
 //The Keyword this
 
 function sayHi() {
-    console.log("Hi")
-    //this refers to the window (global scope object in the browser)
-    console.log(this);
+  console.log("Hi");
+  //this refers to the window (global scope object in the browser)
+  console.log(this);
 }
 
 const greet = function () {
-    console.log(this);
-}
+  console.log(this);
+};
 
 // const person = {
 //     first: 'Cherilyn',
@@ -20,46 +20,48 @@ const greet = function () {
 // }
 
 const person = {
-    first: 'Cherilyn',
-    last: 'Sarkisian',
-    nickName: 'Cher',
-    fullName() {
-        //In a method, this refers to the object the method "lives in:"
-        const {
-            first,
-            last,
-            nickName
-        } = this;
-        return`${first} ${last} AKA ${nickName}`;
-    },
-    printBio(){
-        console.log(this);
-        const fullName = this.fullName();
-        console.log(`${fullName} is a person!`)
-    },
-    laugh: () => {
-        console.log(this);
-        console.log(`${this.nickName} says HAHAHAHA`)
-    }
-}
+  first: "Cherilyn",
+  last: "Sarkisian",
+  nickName: "Cher",
+  fullName() {
+    //In a method, this refers to the object the method "lives in:"
+    const { first, last, nickName } = this;
+    return `${first} ${last} AKA ${nickName}`;
+  },
+  printBio() {
+    console.log(this);
+    const fullName = this.fullName();
+    console.log(`${fullName} is a person!`);
+  },
+  laugh: () => {
+    console.log(this);
+    console.log(`${this.nickName} says HAHAHAHA`);
+  },
+};
 
 const printBio = person.printBio;
 
-
 const annoyer = {
-    phrases: ["literally", "cray cray", "I can't even", "Totes!", "YOLO", "Can't Stop, Won't Stop"],
-    pickPhrase(){
-        const {phrases} = this;
-       const idx = Math.floor(Math.random() * phrases.length)
-        return phrases[idx]
-    },
-    start(){
-        this.timerId = setInterval(() => {
-            console.log(this.pickPhrase())
-        }, 3000)
-    },
-    stop() {
-        clearInterval(this.timerId);
-        console.log("Phew thank heavens that is over!")
-    }
-} 
+  phrases: [
+    "literally",
+    "cray cray",
+    "I can't even",
+    "Totes!",
+    "YOLO",
+    "Can't Stop, Won't Stop",
+  ],
+  pickPhrase() {
+    const { phrases } = this;
+    const idx = Math.floor(Math.random() * phrases.length);
+    return phrases[idx];
+  },
+  start() {
+    this.timerId = setInterval(() => {
+      console.log(this.pickPhrase());
+    }, 3000);
+  },
+  stop() {
+    clearInterval(this.timerId);
+    console.log("Phew thank heavens that is over!");
+  },
+};
